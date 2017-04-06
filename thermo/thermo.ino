ec6990f22c31fef921a80da447f146979b821abb
Michal Nezerka <michal.nezerka@gmail.com>
2017-04-06 15:39:37 +0200

    Use of timer + use of wifi manager

--- thermo/thermo.ino
+++ thermo/thermo.ino
@@ -1,48 +1,76 @@
+#include "Timer.h" // http://playground.arduino.cc/Code/Timer#Installation
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <WiFiManager.h>
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 5
 // How many bits to use for temperature values: 9, 10, 11 or 12
-#define SENSOR_RESOLUTION 12
-// Index of sensors connected to data pin, default: 0
-#define SENSOR_INDEX 0
+#define SENSOR_RESOLUTION 9
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 
 // Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+DallasTemperature dallas(&oneWire);
 DeviceAddress sensorDeviceAddress;
+
+Timer t;
 
 void setup(void)
 {
   // start serial port
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
+  Serial.println("Dallas Temperature IC Control");
 
-  // Start up the library
-  sensors.begin();
-  sensors.getAddress(sensorDeviceAddress, 0);
-  sensors.setResolution(sensorDeviceAddress, SENSOR_RESOLUTION);
+  // Dallas
+  dallas.begin();
+  dallas.getAddress(sensorDeviceAddress, 0);
+  dallas.setResolution(sensorDeviceAddress, SENSOR_RESOLUTION);
+
+  //WiFi manager
+  Serial.println("Starting WiFi manager...");
+  setupWifi();
+
+  //Timer
+  t.every(1000, takeReading);
 }
+
+void setupWifi()
+{
+  WiFiManager wifiManager;
+  wifiManager.setTimeout(300);
+  wifiManager.autoConnect("ESP8266 Settings");
+}
+
+
+
+//////////////////////////// LOOP /////////////////////////////
 
 void loop(void)
 { 
+  t.update();
+}
+
+void takeReading()
+{
+  //lastExecutionTime = millis();
+
   // call sensors.requestTemperatures() to issue a global temperature 
   // request to all devices on the bus
   Serial.println("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
 
-  float temperatureInCelsius = sensors.getTempCByIndex(SENSOR_INDEX);
-  float temperatureInFahrenheit = sensors.getTempFByIndex(SENSOR_INDEX);
+  dallas.requestTemperatures();
+  float dallas1 = dallas.getTempCByIndex(0);
 
-  Serial.print("C Temperature for the device 1 is: ");
-  Serial.println(temperatureInCelsius, 4);
-  Serial.print("F Temperature for the device 1 is: ");
-  Serial.println(temperatureInFahrenheit, 4);
-  
-  //Serial.println(sensors.getTempCByIndex(SENSOR_INDEX));  
+  if (dallas1 == -127) dallas1 = NAN;
 
-  delay(10000);
+  if (dallas1 == NAN)
+  {
+    Serial.println("Failed to read from Dallas sensor!");
+    return;
+  }  
+
+  Serial.println("Dallas | Measured data: " + String(dallas1) + "°C");
 }
+
