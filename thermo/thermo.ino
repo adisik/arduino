b33934fb41e98225ee7994dd1801d7d800718f12
Michal Nezerka <michal.nezerka@gmail.com>
2017-04-06 16:43:10 +0200

    Added web server

--- thermo/thermo.ino
+++ thermo/thermo.ino
@@ -1,3 +1,6 @@
+//#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 #include "Timer.h" // http://playground.arduino.cc/Code/Timer#Installation
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -17,6 +20,10 @@ DeviceAddress sensorDeviceAddress;
 
 Timer t;
 
+ESP8266WebServer httpServer(80);
+
+String host;
+
 void setup(void)
 {
   // start serial port
@@ -24,13 +31,18 @@ void setup(void)
   Serial.println("Dallas Temperature IC Control");
 
   // Dallas
+  Serial.println("Initialize Dallas sensors ...");
   dallas.begin();
   dallas.getAddress(sensorDeviceAddress, 0);
   dallas.setResolution(sensorDeviceAddress, SENSOR_RESOLUTION);
 
-  //WiFi manager
-  Serial.println("Starting WiFi manager...");
+  // WiFi manager
+  Serial.println("Initialize WiFi manager...");
   setupWifi();
+
+  // Http Server
+  Serial.println("Initialize WebServer...");
+  setupWebServer();
 
   //Timer
   t.every(1000, takeReading);
@@ -43,6 +55,20 @@ void setupWifi()
   wifiManager.autoConnect("ESP8266 Settings");
 }
 
+void setupWebServer()
+{
+  char login[20], passwd[20];
+  
+  MDNS.begin(host.c_str());
+  //httpUpdater.setup(&httpServer, update_path, update_username.c_str(), update_password.c_str());
+  httpServer.on("/", HTTP_GET, []() {
+    httpServer.sendHeader("Connection", "close");
+    httpServer.sendHeader("Access-Control-Allow-Origin", "*");
+    httpServer.send(200, "text/html", deviceStatus());
+  });
+  httpServer.begin();
+  MDNS.addService("http", "tcp", 80);
+}
 
 
 //////////////////////////// LOOP /////////////////////////////
@@ -52,16 +78,23 @@ void loop(void)
   t.update();
 }
 
-void takeReading()
-{
-  //lastExecutionTime = millis();
+void takeReading() {
 
+  float dallas1;
+  readDallas(dallas1);
+  
+  Serial.println("Dallas | Measured data: " + String(dallas1) + "°C");
+}
+
+
+void readDallas(float &dallas1)
+{
   // call sensors.requestTemperatures() to issue a global temperature 
   // request to all devices on the bus
   Serial.println("Requesting temperatures...");
 
   dallas.requestTemperatures();
-  float dallas1 = dallas.getTempCByIndex(0);
+  dallas1 = dallas.getTempCByIndex(0);
 
   if (dallas1 == -127) dallas1 = NAN;
 
@@ -70,7 +103,59 @@ void takeReading()
     Serial.println("Failed to read from Dallas sensor!");
     return;
   }  
-
-  Serial.println("Dallas | Measured data: " + String(dallas1) + "°C");
 }
 
+String deviceStatus()
+{
+    String info;
+
+    info  = "<!DOCTYPE HTML><html><head><title>ESP8266 device</title><meta charset='utf-8' />";
+    info += "<style>html{font-family: sans-serif}</style></head>";
+    info += "<body><h1>ESP8266 device</h1>";
+
+    info += ("<b>Hostname:</b> ");
+    info += (host + ".local [<a href='//" + host + ".local'>open</a>]");
+    info += ("<br />");
+
+    info += ("<b>IP address:</b> ");
+    info += (WiFi.localIP().toString());
+    info += ("<br />");
+
+    byte mac[6];
+
+    info += ("<b>MAC address:</b> ");
+    info += mac2String(WiFi.macAddress(mac));
+    info += ("<br />");
+
+    info += ("<b>WiFi network (SSID):</b> ");
+    info += (WiFi.SSID());
+    info += ("<br />");
+
+    info += ("<b>Signal strength (RSSI):</b> ");
+    info += (WiFi.RSSI());
+    info += (" dBm");
+    info += ("<br />");
+
+    Serial.println();
+    float dallas1;
+    readDallas(dallas1);
+    
+    info += ("<b>DS18B20 Readings:</b> " + String(dallas1) + "&#8451;<br />");
+
+    info += ("</body></html>");
+
+    return info;
+}
+
+String mac2String(byte ar[])
+{
+  String s;
+  for (byte i = 0; i < 6; ++i)
+  {
+    char buf[3];
+    sprintf(buf, "%2X", ar[i]);
+    s += buf;
+    if (i < 5) s += ':';
+  }
+  return s;
+}
